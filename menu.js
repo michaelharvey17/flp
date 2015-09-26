872e9fdbf8818ed60a4d7ad1e54c382d7d02d092
Michael Harvey <michaelharvey17@gmail.com>
2015-09-26 15:42:16 -0400

    added new icons and favorite and download buttons.

--- menu.js
+++ menu.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   $(".id").click(function(){
     $(".menu-wrapper").animate({
-      left: "+=620px"
+      left: "+=622.5px",
     }, 1000, function(){
     });
   });
 
   $(".back-button, .activities").click(function(){
     $(".menu-wrapper").animate({
-      left: "-=620px"
+      left: "-=622.5px",
     }, 1000, function(){
     });
   });
